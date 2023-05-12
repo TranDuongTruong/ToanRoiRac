42ee269367ed6c7532449258122c9d7e37ab3c95
unknown <tranduongtruong1623@gmail.com>
2023-05-12 16:30:16 +0700

    Chung1

---
+++ Chuong1/BaiToanChuong1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+int n=3;
+char A='A', B='B',C='C';
+
+void chuyen(int n, char A, char B, char C){
+	if(n>1){
+		chuyen(n-1,A,C,B);
+		chuyen(1,A,B,C);
+		chuyen(n-1,B,A,C);
+	}
+	else{
+		cout<<A<<"->"<<C<<endl;
+	}
+}
+int main(int argc, char** argv){
+	chuyen(3,A,B,C);
+	return 0;
+}
